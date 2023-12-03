b5b41f0f1127c32faac0867516b36929f172423c
tommykrueger <info@tommykrueger.com>
2023-12-03 22:01:16 +0100

    feat: change order of getTitle() public function in Functions.php

--- Framework/Functions.php
+++ Framework/Functions.php
@@ -84,10 +84,10 @@ function getTitle($data): string
 {
     if (has($data, 'seo_title')) {
         $string = $data->seo_title;
-    } elseif (has($data, 'name')) {
-        $string = $data->name;
-    } else {
+    } elseif (has($data, 'title')) {
         $string = $data->title ?? '';
+    } else {
+        $string = $data->name;
     }
     return encode($string);
 }
